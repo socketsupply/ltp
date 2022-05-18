57ee3debf11f82c6b5b91e16ed4af8839e0b0329
Dominic Tarr <dominic.tarr@gmail.com>
2022-05-18 17:29:42 +1200

    move c utils out of wasm, because zig header doesnt include those

--- test/wasm.js
+++ test/wasm.js
@@ -4,6 +4,14 @@ var ltp = require('../index')
 var crypto = require('crypto')
 var tape = require('tape')
 
+function memcpy (dst, from, len) {
+  memory.copy(memory, dst, from, from+len)
+}
+
+function strlen (cstr) {
+  for(var i = 0; 0 != memory[cstr+i] && i < memory.length; i++) ;
+  return i
+}
   
 var O = ltp.ObjectCodec([
   ltp.DirectField('foo', 0, ltp.codex.u8),
@@ -36,9 +44,9 @@ tape('init', function (t) {
 
   }).then(function (_module) {
     module = _module
-    memory = Buffer.from(module.instance.exports.memory.buffer)
-    start = module.instance.exports.__heap_base.value
     wasm = module.instance.exports
+    memory = Buffer.from(wasm.memory.buffer)
+    start = wasm.__heap_base ? wasm.__heap_base.value : 10_000;
     t.end()
   })
 })
@@ -53,7 +61,7 @@ tape('read raw data', function (t) {
 
   var length = O.encode(expected, memory, start)
   baz = start+O.encode.bytes
-  ltp.codex.string_u8.encode('baz', memory, wasm._strlen(baz)+1, baz)
+  ltp.codex.string_u8.encode('baz', memory, strlen(baz)+1, baz)
   console.log(memory.slice(start, start+30))
 
 
@@ -80,7 +88,8 @@ function decode_string(ptr) {
 // uses generated methods to read pointers, and generic methods to read values.
 // (could use generated methods here that do not suffix the field type)
 // decode__basic_name__length ???
-tape('read via generated apis', function (t) {
+tape.skip('read via generated apis', function (t) {
+  
   t.equal(wasm.decode__basic_foo(start), expected.foo)
   t.equal(wasm.decode__basic_bar(start), expected.bar)
   t.equal(decode_string(wasm.decode__basic_name(start)), expected.name)
@@ -154,9 +163,14 @@ tape('encode via C', function (t) {
   console.log(memory.slice(string_u8, string_u8+5))
   console.log(decode_string(string_u8))
 */
-  free += wasm.encode__basic_name(start, wasm._strlen(cstring)+1, cstring, free)
-
-  console.log("DECODE NAME")
+  var _len = wasm.encode__basic_name(start, strlen(cstring)+1, cstring, free)
+  console.log(_len, strlen(cstring)+1)
+  console.log(memory.slice(free, free+_len))
+  free += _len
+//  free += len
+  t.equal(_len, 1+6)
+  
+  console.log("DECODE NAME", memory.slice(start, free))
   t.equal(O.decode(memory, start).name, 'HELLO')
   t.equal(free, start+len+8+1+5+1)
   t.end()
@@ -192,10 +206,10 @@ tape('encode via C & compact', function (t) {
   var cstring2 = cstring+1000
 
   function decode_cstring(s) {
-    return memory.slice(s, s + wasm._strlen(s)).toString('utf8')
+    return memory.slice(s, s + strlen(s)).toString('utf8')
   }
 
-  wasm._memcpy(cstring2, cstring, wasm._strlen(cstring)+1)
+  memcpy(cstring2, cstring, strlen(cstring)+1)
   console.log('start-cstring2', decode_cstring(cstring2))
   
 
@@ -210,8 +224,8 @@ tape('encode via C & compact', function (t) {
   console.log('start-cstring', decode_cstring(cstring))
 //  console.log('decode__basic_name',  wasm.encode__basic_name(start))
   console.log('raw', memory.slice(start, free+10))
-  free += wasm.encode__basic_name(start, wasm._strlen(cstring)+1, cstring, free)
-  t.equal(free, start+6+1+5+1, 'free pointer is correct')
+  free += wasm.encode__basic_name(start, strlen(cstring)+1, cstring, free)
+  t.equal(free, start+6+1+5+1, 'free pointer must be correct')
   console.log('raw', memory.slice(start, free+10))
   var cstring3 = wasm.decode__basic_name(start)
   console.log('start-cstring2', decode_cstring(cstring2))
@@ -238,9 +252,9 @@ tape('single encode call', function (t) {
   var string = 'HI THERE\x00'
   start2 += memory.write(string, cstring2=start2, 'utf8')
   console.log('hi there', memory.slice(cstring2, cstring2+10))
-  t.equal(wasm._strlen(cstring2), string.length-1, "correct string length")
-  console.log(wasm._strlen(cstring2))
-  var bytes = wasm.encode__simpler(start2, 100, 1000, wasm._strlen(cstring2)+1, cstring2)
+  t.equal(strlen(cstring2), string.length-1, "correct string length")
+  console.log(strlen(cstring2))
+  var bytes = wasm.encode__simpler(start2, 100, 1000, strlen(cstring2)+1, cstring2)
   console.log(memory.slice(start2, start2+32))
   console.log(bytes)
   t.deepEqual(S.decode(memory, start2), {foo: 100, bar: 1000, name: 'HI THERE'})
@@ -251,7 +265,7 @@ tape('encode/decode string_u32', function (t) {
   var cstring2
   var string = 'LTP\x00'
   memory.write(string, cstring2=start2, 'utf8')
-  var bytes = wasm.encode__bigName(start, wasm._strlen(cstring2)+1, cstring2)
+  var bytes = wasm.encode__bigName(start, strlen(cstring2)+1, cstring2)
   console.log(memory.slice(start, start+32))
 //  t.deepEqual(BN.decode(memory, start2), {name: 'HI THERE'})
   t.end()
