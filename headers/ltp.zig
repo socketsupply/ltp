643aa6128e7232874f08e3324e79f1da104aad07
Dominic Tarr <dominic.tarr@gmail.com>
2022-05-19 21:32:57 +1200

    export the utility methods from headers/ltp.zig

--- headers/ltp.zig
+++ headers/ltp.zig
@@ -10,33 +10,33 @@ fn encode_n (comptime T:anytype, buf:[*]u8, value: T) void {
 
 // integers
 
-export fn ltp_decode__u8  (buf: [*]u8) u8  { return decode_n(u8, buf); }
-export fn ltp_decode__u16 (buf: [*]u8) u16 { return decode_n(u16, buf); }
-export fn ltp_decode__u32 (buf: [*]u8) u32 { return decode_n(u32, buf); }
-export fn ltp_decode__u64 (buf: [*]u8) u64 { return decode_n(u64, buf); }
+pub export fn ltp_decode__u8  (buf: [*]u8) u8  { return decode_n(u8, buf); }
+pub export fn ltp_decode__u16 (buf: [*]u8) u16 { return decode_n(u16, buf); }
+pub export fn ltp_decode__u32 (buf: [*]u8) u32 { return decode_n(u32, buf); }
+pub export fn ltp_decode__u64 (buf: [*]u8) u64 { return decode_n(u64, buf); }
 
-export fn ltp_decode__i8  (buf: [*]u8) i8  { return decode_n(i8, buf); }
-export fn ltp_decode__i16 (buf: [*]u8) i16 { return decode_n(i16, buf); }
-export fn ltp_decode__i32 (buf: [*]u8) i32 { return decode_n(i32, buf); }
-export fn ltp_decode__i64 (buf: [*]u8) i64 { return decode_n(i64, buf); }
+pub export fn ltp_decode__i8  (buf: [*]u8) i8  { return decode_n(i8, buf); }
+pub export fn ltp_decode__i16 (buf: [*]u8) i16 { return decode_n(i16, buf); }
+pub export fn ltp_decode__i32 (buf: [*]u8) i32 { return decode_n(i32, buf); }
+pub export fn ltp_decode__i64 (buf: [*]u8) i64 { return decode_n(i64, buf); }
 
-export fn ltp_encode__u8  (buf: [*]u8, value: u8)  void { encode_n(u8, buf, value); }
-export fn ltp_encode__u16 (buf: [*]u8, value: u16) void { encode_n(u16, buf, value); }
-export fn ltp_encode__u32 (buf: [*]u8, value: u32) void { encode_n(u32, buf, value); }
-export fn ltp_encode__u64 (buf: [*]u8, value: u64) void { encode_n(u64, buf, value); }
+pub export fn ltp_encode__u8  (buf: [*]u8, value: u8)  void { encode_n(u8, buf, value); }
+pub export fn ltp_encode__u16 (buf: [*]u8, value: u16) void { encode_n(u16, buf, value); }
+pub export fn ltp_encode__u32 (buf: [*]u8, value: u32) void { encode_n(u32, buf, value); }
+pub export fn ltp_encode__u64 (buf: [*]u8, value: u64) void { encode_n(u64, buf, value); }
 
-export fn ltp_encode__i8  (buf: [*]u8, value: i8)  void { encode_n(i8,  buf, value); }
-export fn ltp_encode__i16 (buf: [*]u8, value: i16) void { encode_n(i16, buf, value); }
-export fn ltp_encode__i32 (buf: [*]u8, value: i32) void { encode_n(i32, buf, value); }
-export fn ltp_encode__i64 (buf: [*]u8, value: i64) void { encode_n(i64, buf, value); }
+pub export fn ltp_encode__i8  (buf: [*]u8, value: i8)  void { encode_n(i8,  buf, value); }
+pub export fn ltp_encode__i16 (buf: [*]u8, value: i16) void { encode_n(i16, buf, value); }
+pub export fn ltp_encode__i32 (buf: [*]u8, value: i32) void { encode_n(i32, buf, value); }
+pub export fn ltp_encode__i64 (buf: [*]u8, value: i64) void { encode_n(i64, buf, value); }
 
 // floats
 
-export fn ltp_decode__f32 (buf: [*]u8) f32 { return @bitCast(f32, decode_n(u32, buf)); }
-export fn ltp_decode__f64 (buf: [*]u8) f64 { return @bitCast(f64, decode_n(u64, buf)); }
+pub export fn ltp_decode__f32 (buf: [*]u8) f32 { return @bitCast(f32, decode_n(u32, buf)); }
+pub export fn ltp_decode__f64 (buf: [*]u8) f64 { return @bitCast(f64, decode_n(u64, buf)); }
 
-export fn ltp_encode__f32 (buf: [*]u8, value: f32) void { encode_n(u32, buf, @bitCast(u32, value)); }
-export fn ltp_encode__f64 (buf: [*]u8, value: f64) void { encode_n(u64, buf, @bitCast(u64, value)); }
+pub export fn ltp_encode__f32 (buf: [*]u8, value: f32) void { encode_n(u32, buf, @bitCast(u32, value)); }
+pub export fn ltp_encode__f64 (buf: [*]u8, value: f64) void { encode_n(u64, buf, @bitCast(u64, value)); }
 
 // relative pointers
 
